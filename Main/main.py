972338cf6f13d4b938c08001e108e9d2d95ab1d3
MundaneHassan <88006430+MundaneHassan@users.noreply.github.com>
2021-09-29 11:39:23 +0300

    Add files via upload

---
+++ Main/main.py
@@ -0,0 +1,105 @@
+from cryptography.fernet import Fernet
+from secrets import choice
+
+
+def generate_sum():
+    sum = Fernet.generate_key()
+    with open("sum.key", "wb") as sum_file:
+        sum_file.write(sum)
+generate_sum()
+
+def create_pin():
+    pid = input("Enter your pin")
+    with open("PID.key", 'wb') as pid_file:
+        pid_file.write(fer.encrypt(pid.encode()))
+create_pin()
+
+'''
+def get_sum():
+    file = open("sum.key", "rb")
+    key_1 = file.read()
+    file.close()
+    return key_1
+
+
+key = get_sum()
+fer = Fernet(key)
+
+
+def get_pin(pin: str):
+    with open("PID.key", "rb") as pid_file:
+        for line in pid_file.readlines():
+            data = bytes(line.rstrip())
+            line1 = fer.decrypt(data).decode()
+    if pin == line1:
+        pass
+    else:
+        exit()
+
+
+get_pin(input("Please enter your pin here :  "))
+
+
+def write_password():
+    username = input("Enter your username here :  ")
+    password = input("Enter your password here, you can type 'generate' to get one : ")
+
+    if password == 'generate':
+        ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+        ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        digits = '0123456789'
+        punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+        combined = ascii_lowercase + ascii_uppercase + digits + punctuation
+        temporary = ''.join(choice(combined) for _ in range(12))
+        password = temporary
+    else:
+        pass
+
+    website_name = input("Enter the website here :  ")
+    email_address = input("Enter the email here : ")
+
+    file_write = open("password.txt", "a")
+    file_write.write(
+        fer.encrypt(username.encode()).decode() + "\n" + fer.encrypt(password.encode()).decode() + "\n" + fer.encrypt(
+            email_address.encode()).decode() + "\n" + fer.encrypt(website_name.encode()).decode() + "\n")
+    file_write.close()
+    print("\nPassword successfully stored and encrypted.")
+
+    another_password = input("Would you like to write another one?\n")
+    if another_password == 'yes' \
+            or another_password == 'y' \
+            or another_password == 'Yes'\
+            or another_password == 'Y' \
+            or another_password == 'w' \
+            or another_password == 'W'\
+            or another_password == 'write' \
+            or another_password == 'Write':
+        write_password()
+    else:
+        exit()
+
+
+def read_password():
+    print("")
+    file_read = open("password.txt", 'rb')
+    content = file_read.readlines()
+    print_counter = 0
+    for i in range(len(content)):
+        print(fer.decrypt(content[i]).decode())
+        print_counter += 1
+        if (print_counter & 3) == 0:
+            print("")
+    file_read.close()
+
+
+welcome = input("\nWelcome, would you like to write or read a password today?\n"
+                "Please type 'W' for write and 'R' for read :  ")
+
+if welcome == 'W' or welcome == 'w' or welcome == 'write' or welcome == 'Write':
+    write_password()
+elif welcome == 'R' or welcome == 'r' or welcome == 'read' or welcome == 'Read':
+    read_password()
+else:
+    print('\nIncorrect input')
+    exit()
+'''
