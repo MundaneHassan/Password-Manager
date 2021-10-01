535608305f230dfe5bfec5e63fef7f9a374a0e50
MundaneHassan <88006430+MundaneHassan@users.noreply.github.com>
2021-10-01 18:56:39 +0300

    Update main.py

--- Main/main.py
+++ Main/main.py
@@ -1,47 +1,54 @@
 from cryptography.fernet import Fernet
-import os, pickle, secrets, sys
+import os
+import pickle
+import secrets
+import sys
+
 
 class PasswordManager:
 
     def __init__(self):
         """
-        `__init__()` 
+        `__init__()`
         if `sum.bin` or `PID.bin` not in current directory then,
         will create file(s) and store the require data.
         and run the Initial code.
         """
-        if ("sum.bin" not in os.listdir()):
+        if "sum.bin" not in os.listdir():
             self.__generate_sum()
-            
+
         self.__get_sum_key()
 
-        if ("PID.bin" not in os.listdir()):
+        if "PID.bin" not in os.listdir():
             self.__create_pin()
 
-
         while True:
-            
+
             self.__get_pin()
 
-            __welcome = input("\nWelcome, would you like to write or read a password today?\nPlease type 'W' for write and 'R' for read\n- ").lower()
+            __welcome = input(
+                "\nWelcome, would you like to write or read a password today?\n"
+                "Please type 'W' for write and 'R' for read\n- ").lower()
 
             # if user entered wrong input
             while not ((__welcome[0] in "wy") or (__welcome[0] == "r")):
                 print("Incorrect Input!")
-                __welcome = input("\nWelcome, would you like to write or read a password today?\nPlease type 'W' for write and 'R' for read\n- ").lower()
-            
-            if (__welcome[0] in "wy"):
+                __welcome = input(
+                    "\nWelcome, would you like to write or read a password today?\n"
+                    "Please type 'W' for write and 'R' for read\n- ").lower()
+
+            if __welcome[0] in "wy":
                 self.__store_details()
-            elif (__welcome[0] == "r"):
+            elif __welcome[0] == "r":
                 self.__print_details()
-            
+
             # If user want to do more work
 
             more = input("Want to do more operations (y/n)?- ").lower()
-            if (more[0] != "y"):
+            if more[0] != "y":
                 print("Program Closed!")
                 break
-        
+
     def __generate_sum(self) -> None:
         """
         `generate_sum()`
@@ -52,7 +59,7 @@ class PasswordManager:
 
         with open("sum.bin", "wb") as __sum_file:
             pickle.dump(__sum_key, __sum_file)
-        
+
     def __create_pin(self) -> None:
         """
         `create_pin()`
@@ -63,13 +70,12 @@ class PasswordManager:
         __encrypted_key = self.__fer.encrypt(__pin.encode())
 
         with open("PID.bin", "wb") as __pid_file:
-            
             pickle.dump(__encrypted_key, __pid_file)
-    
+
     def __get_sum_key(self) -> None:
         """
         `get_sum_key()`
-        loads the key from `sum.bin` 
+        loads the key from `sum.bin`
         and assigns the value of key to `self.__fer`.
         """
 
@@ -92,17 +98,16 @@ class PasswordManager:
         with open("PID.bin", "rb") as __pid_file:
             __saved_pin = self.__fer.decrypt(pickle.load(__pid_file)).decode()
 
-        while (__pin != __saved_pin):
+        while __pin != __saved_pin:
             __attempt -= 1
-            if (__attempt <= 0):
+            if __attempt <= 0:
                 print("Your chances are over!")
                 print("Program Closed!")
                 sys.exit()
-            
+
             print("You entered wrong pin!")
             print(f"You have only {__attempt} attempts left!")
             __pin = input("Please enter your pin here: ")
-
 
     def __store_details(self) -> None:
         """
@@ -116,7 +121,7 @@ class PasswordManager:
         __password = input("Enter your password here, you can type 'generate' to get one: ")
 
         # if user wanted to make password
-        if (__password.lower() == "generate" or __password.lower() == "generates"):
+        if __password.lower() == "generate":
             self._ASCIIs = [f"{chr(i)}" for i in range(31, 127)]
             __password = "".join([secrets.choice(self._ASCIIs) for _ in range(12)])
             print("Password created Successfully!\n")
@@ -132,13 +137,13 @@ class PasswordManager:
         # storing the details
         with open("Sign.bin", "ab") as __sign_file:
             pickle.dump(__encrypted_stuff, __sign_file)
-        
+
         print("\nPassword successfully stored and encrypted.")
-    
+
     def __print_details(self) -> None:
         """
         `print_details()`
-        will laod the encrypted details from `Sign.bin` binary file
+        will load the encrypted details from `Sign.bin` binary file
         and decrypt and print them.
         """
         try:
@@ -147,7 +152,7 @@ class PasswordManager:
             print("No details are stored yet!")
             return
         except Exception as __e:
-          print(f"Unkown Error occured:\n{__e}")
+            print(f"Unknown Error occurred:\n{__e}")
 
         try:
             while True:
@@ -155,20 +160,20 @@ class PasswordManager:
                 __encrypted_details = pickle.load(__sign_file)
 
                 __names = ["Name", "Password", "Email", "Website"]
-                
+
                 __details = [self.__fer.decrypt(i).decode() for i in __encrypted_details]
-                
+
                 # printing the details
-                print(f"{'-'*40}\n{'New group':^40}\n{'-'*40}")
-                
+                print(f"{'-' * 40}\n{'New group':^40}\n{'-' * 40}")
+
                 for __name, __detail in zip(__names, __details):
                     print(f"{__name:^15} : {__detail:^15}")
-                print("-"*60)
-        except EOFError:    # if file ends
+                print("-" * 60)
+        except EOFError:  # if file ends
             __sign_file.close()
             print("End of Details")
         except Exception as __e:
-          print(f"Unkown Error occured:\n{__e}")
+            print(f"Unknown Error occurred:\n{__e}")
 
 
 if __name__ == "__main__":
